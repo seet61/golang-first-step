726670990da9d7a7cb1e47f68bfe353a80fbd82f
seet61 <seet_seet@mail.ru>
2019-10-20 12:56:22 +0300

    Добавил примеры по главам 2 - 4

---
+++ chapter3/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("1 + 1 =", 1.0+2.0)
+	fmt.Println("Hello World"[1])
+	fmt.Println("length of string:", len("Hello World"))
+}
---
+++ chapter4/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string
+	x = "hello"
+	fmt.Println(x)
+	x = "world"
+	fmt.Println(x)
+}
